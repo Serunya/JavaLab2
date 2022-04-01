0d4c425de1766d9301130fdf92f145170664f666
TailSpin <37714042+Serunya@users.noreply.github.com>
2022-04-01 08:54:28 +0300

    m

--- src/Gorner.java
+++ src/Gorner.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Gorner {
     public static void main(String[] args) {
-        Double[] ratio = {5.,8.,4.,2.};
-
+        double[] P = {2,7,1,4,5};
+        schem(2,P);
+    }
+    public static void schem(double x, double[] P){
+        int n = P.length - 1;
+        double result = P[n] * x + P[n-1];
+        for(int i = n - 1; i >= 0; i--){
+            result = result * x + P[i];
+        }
+        System.out.println(result);
     }
 }
+
--- src/MaxNegative.java
+++ src/MaxNegative.java
@@ -2,6 +2,7 @@ public class MaxNegative {
     public static void main(String[] args) {
         int[][] mas = {{12,21},{42,12,12,44},{25,12},{41,-25}};
         int max = Integer.MIN_VALUE;
+        boolean flag = false;
         for(int i = 0; i < mas.length;i++){
             for(int j = 0; j < mas[i].length;j++){
                 if(mas[i][j] < 0 && mas[i][j] > max){
